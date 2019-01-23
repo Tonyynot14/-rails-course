a17ce1e684ba4bf308caa9969fcd7492c8fa3712
Tony Wade <wadant3@gmail.com>
2019-01-23 22:15:34 +0000

    All commits for contacts app

--- contacts/config/routes.rb
+++ contacts/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/contacts" => "contacts#index"
+  get "/contacts/new" => "contacts#new"
+  post "contacts" => "contacts#create"
+  get "contacts/edit" => "contacts#edit"
+  post "contacts/edit" =>"contacts#editInfo"
+  get "contacts/search" =>"contacts#search"
+  get "contacts/search_results" => "contacts#search_results"
 end
