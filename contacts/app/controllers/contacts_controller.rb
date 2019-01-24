a62d3649699a33bff7118e8acff03c5dd7910f64
Tony Wade <wadant3@gmail.com>
2019-01-24 20:16:43 +0000

    Changed from two seperate search boxes to one

--- contacts/app/controllers/contacts_controller.rb
+++ contacts/app/controllers/contacts_controller.rb
@@ -5,43 +5,42 @@ class ContactsController < ApplicationController
     def new
     end
     def create
-     contact = Contact.new
-       contact.first_name = params[:first_name]
-       contact.last_name = params[:last_name]
-       contact.phone = params[:phone]
-       contact.email = params[:email]
-       contact.address = params[:address]
-       contact.city = params[:city]
-       contact.state = params[:state]
-       contact.zip = params[:zip]
-       contact.save
-       redirect_to '/contacts'
+        contact = Contact.new
+        contact.first_name = params[:first_name]
+        contact.last_name = params[:last_name]
+        contact.phone = params[:phone]
+        contact.email = params[:email]
+        contact.address = params[:address]
+        contact.city = params[:city]
+        contact.state = params[:state]
+        contact.zip = params[:zip]
+        contact.save
+        redirect_to '/contacts'
     end
     def edit
-    @contact = Contact.find(params[:id])
+        @contact = Contact.find(params[:id])
     end
     def editInfo
-    contact = Contact.find(params[:id])
-     contact.first_name = params[:first_name]
-     contact.last_name = params[:last_name]
-     contact.phone = params[:phone]
-     contact.email = params[:email]
-     contact.address = params[:address]
-     contact.city = params[:city]
-     contact.state = params[:state]
-     contact.zip = params[:zip]
-     contact.save
-     redirect_to '/contacts'
+        contact = Contact.find(params[:id])
+        contact.first_name = params[:first_name]
+        contact.last_name = params[:last_name]
+        contact.phone = params[:phone]
+        contact.email = params[:email]
+        contact.address = params[:address]
+        contact.city = params[:city]
+        contact.state = params[:state]
+        contact.zip = params[:zip]
+        contact.save
+        redirect_to '/contacts'
     end
     
     def search
     end
     def search_results
          #need to be able to search by first name full name or last name or
-    @first_name = "%#{params[:first_name]}%"
-    @last_name ="%#{params[:last_name]}%"
-    @contacts = Contact.where("last_name LIKE ? and first_name LIKE ?",@last_name,@first_name)
-    @first_name_no_per = params[:first_name]
-    @last_name_no_per = params[:last_name]
+        name = "%#{params[:name]}%"
+        
+        @contacts = Contact.where("last_name LIKE ? or first_name LIKE ?",name,name)
+        @name = params[:name]
     end
 end
