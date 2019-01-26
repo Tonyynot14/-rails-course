fb6491d06a027d05aed626ad5b955a8b45af9d7b
Tony Wade <wadant3@gmail.com>
2019-01-26 20:05:40 +0000

    Contacts app with search feature added to sintra app

--- Sintra-contacts/app.rb
+++ Sintra-contacts/app.rb
@@ -13,6 +13,53 @@ set :database, { adapter: 'sqlite3', database: 'test.db' }
 #class Question < ActiveRecord::Base
 
 #end
-get '/' do
-    'Hello World'
+class Contact <ActiveRecord::Base
+    
+end
+get '/contacts' do
+    @contacts = Contact.all
+    erb :index
+end
+
+get '/contacts/new' do
+    erb:new
+end
+post '/contacts' do
+    contact = Contact.new
+    contact.first_name = params[:first_name]
+    contact.last_name = params[:last_name]
+    contact.phone = params[:phone]
+    contact.email = params[:email]
+    contact.address = params[:address]
+    contact.city = params[:city]
+    contact.state = params[:state]
+    contact.zip = params[:zip]
+    contact.save
+    redirect '/contacts'
+    end
+get '/contacts/edit' do
+    @contact = Contact.find(params[:id])
+    erb:edit
+end
+post '/contacts/edit' do
+    contact = Contact.find(params[:id])
+    contact.first_name = params[:first_name]
+    contact.last_name = params[:last_name]
+    contact.phone = params[:phone]
+    contact.email = params[:email]
+    contact.address = params[:address]
+    contact.city = params[:city]
+    contact.state = params[:state]
+    contact.zip = params[:zip]
+    contact.save
+    redirect '/contacts'
+end
+    
+get '/contacts/search_results' do
+    #need to be able to search by first name full name or last name or
+    name = "%#{params[:name]}%"
+        
+    @contacts = Contact.where("last_name LIKE ? or first_name LIKE ?",name,name)
+    @name = params[:name]
+    erb:search_results
 end
