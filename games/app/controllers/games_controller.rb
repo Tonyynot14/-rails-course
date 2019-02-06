049da8b9131f5db72a98edbffb3a7b3e088116f4
Tony Wade <wadant3@gmail.com>
2019-02-06 16:31:52 +0000

    finished many to many relationship

---
+++ games/app/controllers/games_controller.rb
@@ -0,0 +1,34 @@
+class GamesController < ApplicationController
+    def index
+        @games = Game.all
+    end
+    def new
+        @game =Game.new
+    end
+    def create
+        game =Game.new
+        game.name=params[:game][:name]
+        game.genre=params[:game][:genre]
+        game.description=params[:game][:description]
+        game.publisher=params[:game][:publisher]
+        game.save
+        redirect_to games_path
+    end
+    def edit
+         @game =Game.find(params[:id])
+    end
+    def update
+        game =Game.find(params[:id])
+        game.name=params[:game][:name]
+        game.genre=params[:game][:genre]
+        game.description=params[:game][:description]
+        game.publisher=params[:game][:publisher]
+        game.save
+        redirect_to games_path
+    end
+    def show
+        @game = game.find(params[:id])
+        @players=Player.all
+    end
+    
+end
